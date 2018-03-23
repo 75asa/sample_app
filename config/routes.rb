c52fa916e8e048f42ebc9f09d90fd13f997d3d58
nuasKent <nuaskent@gmail.com>
2018-03-23 20:44:14 +0900

    Done layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
+  # get 'users/new'
+  # get '/new', to: 'users#new'
+  # get '/users', to:'users'
+  # get '/signup', to: '/users/signup'
+
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
 end
